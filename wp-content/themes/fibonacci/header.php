e08f8bf10617822357ff491e6b55000d251e82ad
Byron Fay <bfay_git@me.com>
2012-08-25 03:43:23 -0400

    set up header and file structure

--- wp-content/themes/fibonacci/header.php
+++ wp-content/themes/fibonacci/header.php
@@ -17,6 +17,8 @@
 	<?php if (is_search()) { ?>
 	<meta name="robots" content="noindex, nofollow" /> 
 	<?php } ?>
+	
+	
 
 	<title>
 		   <?php
@@ -62,13 +64,42 @@
 	<meta name="google-site-verification" content="">
 	<!-- Speaking of Google, don't forget to set your site up: http://google.com/webmasters -->
 	
-	<meta name="author" content="Your Name Here">
-	<meta name="Copyright" content="Copyright Your Name Here 2011. All Rights Reserved.">
+	<!-- mobile meta (hooray!) -->
+		<meta name="HandheldFriendly" content="True">
+		<meta name="MobileOptimized" content="320">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<meta name="google-site-verification" content="Eighth Loop Creative Group">
+	<!-- Speaking of Google, don't forget to set your site up: http://google.com/webmasters -->
+	
+	<meta name="keywords" content="foundation rapid prototyping, zurb foundation 3, design, frameworks, wordpress framework, css framework, html framework, html5, css3 transitions, css3, SASS, SCSS, compass, responsive design, hybrid responsive web design, RWD, HRWD, modernizr, categorizr, selectivizr, how to build a website, CodeKit, Coda 2, web developer, barberton ohio, north canton ohio, akron ohio, cleveland ohio, northeast ohio, web design barberton ohio, web design norton ohio, web design canton ohio, ">
+	<meta name="author" content="Byron Fay">
 
 	<!-- Dublin Core Metadata : http://dublincore.org/ -->
-	<meta name="DC.title" content="Project Name">
-	<meta name="DC.subject" content="What you're about.">
-	<meta name="DC.creator" content="Who made this site.">
+	<meta name="DC.title" content="Learning to Build a Hybrid Responsive Website Design (HRWD)">
+	<meta name="DC.subject" content="A web designer & developer full of youthful exuberance and curiousity which cannot be satisfied">
+	<meta name="DC.creator" content="Byron Fay">  
+				
+	<!-- Favicon and Feed -->
+	<link rel="shortcut icon" type="image/png" href="http://8images.eighthloop.com/favicon.png">
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+	
+	<!--  iPhone Web App Home Screen Icon -->
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/images/apple_icon_72.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/apple_icon_114.png" />
+	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/apple_icon_58.png" />
+	
+	<!-- Enable Startup Image for iOS Home Screen Web App -->
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/apple_icon_58.png" />
+
+
+	<!-- Startup Image iPad Landscape (748x1024) -->
+	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/images/ipad_landscape_loading.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)" />
+	<!-- Startup Image iPad Portrait (768x1004) -->
+	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/images/ipad_portrait_loading.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)" />
+	<!-- Startup Image iPhone (320x460) -->
+	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/images/iphone_load.png" media="screen and (max-device-width: 320px)" />
+
 	
 	<!--  Mobile Viewport meta tag
 	j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
@@ -79,13 +110,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	-->
 	
-	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
-	<!-- This is the traditional favicon.
-		 - size: 16x16 or 32x32
-		 - transparency is OK
-		 - see wikipedia for info on browser support: http://mky.be/favicon/ -->
-		 
-	<link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/_/img/apple-touch-icon.png">
 	<!-- The is the icon for iOS's Web Clip.
 		 - size: 57x57 for older iPhones, 72x72 for iPads, 114x114 for iPhone4's retina display (IMHO, just go ahead and use the biggest one)
 		 - To prevent iOS from applying its styles to the icon name it thusly: apple-touch-icon-precomposed.png
@@ -97,9 +121,34 @@
 	<!-- all our JS is at the bottom of the page, except for Modernizr. -->
 	<script src="<?php bloginfo('template_directory'); ?>/_/js/modernizr-1.7.min.js"></script>
 	
+	
+	
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+	
+	
+	<!-- ! Typekit Fonts being used -->
+	 <script type="text/javascript" src="//use.typekit.net/jky1bld.js"></script>
+	 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+	
+	
+					<!-- drop Google Analytics Here -->
+		<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33695808-1']);
+  _gaq.push(['_setDomainName', 'eighthloop.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+		<!-- end analytics -->
 
 	<?php wp_head(); ?>
 	
